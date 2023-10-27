6bb29fa42e7d2ec6b1cbfcf4de0d4bfe4c5a2662
tiredtiredness <tiredtiredness@gmail.com>
2023-10-27 13:26:01 +0300

    major changes

--- lab2_tpr1.m
+++ lab2_tpr1.m
@@ -1,10 +1,10 @@
 a = input("Введите значение нечеткого числа ");
 power_k = input("Введите степень нечеткости колокольной функции ");
 
-lim = 50;
-num = linspace(-lim, lim, 100);
+num = -50:50;
 s = "Предельное значение ФП " + lim;
 
 kolokol = (1 ./ (1 + power_k^2 * (num - a).^2));
+hold on;
 plot(num, kolokol);
 title("Колокольная функция");
--- main.m
+++ main.m
@@ -1,6 +1,12 @@
 T1 = 'Выполнить действия для L1';
 T2 = 'Выполнить действия для L2';
 T3 = 'Выход';
+T4 = 'Циклическая';
+T5 = 'Программа-функция';
+T6 = 'Разветвленная';
+T7 = 'Замкнутая';
+T8 = 'Гауссова функция';
+T9 = 'Колокольная функция';
 
 while 1
     upr = menu('Выберите действие:', T1, T2, T3);
@@ -8,14 +14,71 @@ while 1
     switch upr
         case 1
             disp('Открыть файлы лабораторной работы №1');
-            run('L1.m'); 
+            l1_menu = menu('Выберите файл из ЛР1:', T4, T5, T6, T7);
+            switch l1_menu
+                case 1
+                    disp('Циклическая');
+                    run("lab2_tpr.m");
+                case 2
+                    disp('Программа-функция');
+                    run("lab2_tpr1.m");
+                case 3
+                    disp('Разветвленная');
+                    run("lab2_tpr1.m");
+                case 4
+                    disp('Замкнутая');
+                    run("lab2_tpr1.m");
+                case 5
+                    disp('Вызов из меню ЛР2.');
+                    break
+            end
 
         case 2
-            disp('Открыть файлы лабораторной работы №1');
-            run('L2.m'); 
-
+            disp('Открыть файлы лабораторной работы №2');
+            l2_menu = menu('Выберите файл из ЛР2:', T8, T9);
+            switch l2_menu
+                case 1
+                    disp('Гауссова функция');
+                    x= -50:50;
+                    for i = 1:3 
+                        for j = 1:3
+                            q=[-5,0,10];
+                            b=[5,8,9];
+                            a=q(i);
+                            sigmaG=b(j);
+                            func = gaussMF(x, a, sigmaG);
+                            hold on;
+                            plot(x, func); 
+                            xlabel('x');
+                            ylabel('func');
+                            title('Гауссова функция');
+                            legend('Гауссова');
+                        end
+                    end
+                case 2
+                    disp('Колокольная функция');
+                    x= -50:50;
+                    for i = 1:3 
+                        for j = 1:3
+                            q=[-5,0,10];
+                            b=[0.05,0.08,0.09];
+                            a=q(i);
+                            sigmaG=b(j);
+                            func = bellMF(x, a, sigmaG);
+                            hold on;
+                            plot(x, func); 
+                            xlabel('x');
+                            ylabel('func');
+                            title('Колокольная функция');
+                            legend('Колокольная');
+                        end
+                    end
+                case 3
+                    disp('Вызов из меню ЛР2.');
+                    break
+            end
         case 3
             disp('Программа завершена.');
-            break
+            return
     end
 end
